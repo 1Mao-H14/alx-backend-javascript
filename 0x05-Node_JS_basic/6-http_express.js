bae4a1af2cbd88a8c8cc9534686a6687ef7c2f0f
marwan <marwanhamdaoui2020@gmail.com>
2025-01-31 11:30:54 -0800

    task ==>6-http_express.js done testing out

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const server = express();
+const serverPort = 1245;
+
+server.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+server.listen(serverPort, () => {});
+
+module.exports = server;
